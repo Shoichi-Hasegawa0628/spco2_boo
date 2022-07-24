375f7cc4cb6d2267405c84dab64e47b042f81a03
Shoichi Hasegawa <hasegawa.shoichi@em.ci.ritsumei.ac.jp>
2022-07-25 04:22:38 +0900

    Fix the number of data

--- rgiro_spco2_slam/src/spco2_learn_concepts_non_gmapping.py
+++ rgiro_spco2_slam/src/spco2_learn_concepts_non_gmapping.py
@@ -1174,7 +1174,7 @@ def Learning(step, filename, particle, XT, ST, W_list, CT, IT, FT, OT, Object_W_
 ########################################
 # def callback(message):
 def callback():
-    N = 20
+    N = 12
     for step in range(1, N+1): # 追加学習するときは、この値を追加するデータ分だけ入れる。
         # trialname = rospy.get_param('~trial_name')
         # datasetNUM = rospy.get_param('~dataset_NUM')
